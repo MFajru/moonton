85b1a42787a94510e0bef0163e84325784fd39fb
MFajru <fajru234@gmail.com>
2022-10-17 18:17:54 +0700

    feat:integration sugn in

--- routes/web.php
+++ routes/web.php
@@ -24,11 +24,11 @@ use Inertia\Inertia;
 //     ]);
 // });
 
-Route::redirect('/', '/prototype/login');
+Route::redirect('/', '/login');
 
-// Route::get('/dashboard', function () {
-//     return Inertia::render('Dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::prefix('prototype')->name('prototype.')->group(function () {
     Route::get('/login', function () {
